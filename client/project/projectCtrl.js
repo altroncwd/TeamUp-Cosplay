4267fc960e29ff6256836ab11fbb6ff9e929f182
Christopher Decker <christopher.w.decker@gmail.com>
2016-04-27 17:27:52 -0700

    [fix] update the controller and tempate

--- client/project/projectCtrl.js
+++ client/project/projectCtrl.js
@@ -1,10 +1,11 @@
 angular
   .module('meetup.project', [])
   .controller('Project', ['$scope', 'ProjectFactory', function ($scope, ProjectFactory) {
-    $scope.test = 'Project Controller refrence';
-
+    $scope.owner = ProjectFactory.project.owner;
     $scope.project = ProjectFactory.project.projectName;
     $scope.deadline = ProjectFactory.project.eventDeadline;
     $scope.deadlineDate = ProjectFactory.project.eventDeadlineDate;
+    $scope.costumes = ProjectFactory.project.costumes;
+
 
   }]);
