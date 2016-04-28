d9bc226ea34915873f617409bb677301ff4b0823
Christopher Decker <christopher.w.decker@gmail.com>
2016-04-27 17:14:18 -0700

    [fix] change the basic data set for testing

--- client/project/projectCtrl.js
+++ client/project/projectCtrl.js
@@ -4,7 +4,7 @@ angular
     $scope.test = 'Project Controller refrence';
 
     $scope.project = ProjectFactory.project.projectName;
-    $scope.deadline = ProjectFactory.project.projectDeadline;
-    $scope.deadlineDate = ProjectFactory.project.projectDeadlineDate;
+    $scope.deadline = ProjectFactory.project.eventDeadline;
+    $scope.deadlineDate = ProjectFactory.project.eventDeadlineDate;
 
   }]);
--- client/project/projectFactory.js
+++ client/project/projectFactory.js
@@ -6,10 +6,53 @@ angular.module('meetup.project.factory', [])
 
 // TEMP TEST DATA: -----------------------------------
   var project = {
-    projectName: "Owari Test",
-    projectDeadline : "AnimeExpo",
-    projectDeadlineDate: "7/1/16"
-  };
+    projectName : 'Owari no Sereph' ,
+    projectOwner : 'Chris',
+    projectMembers : {
+      owner : 'Chris',
+      as7g6d78g9 : 'John'
+    },
+    eventDeadline : "AnimeExpo",
+    eventDeadlineDate : "7/1/16",
+    projectCreated : Date(),
+    costumes : [
+      { character : 'Guren',
+        refrenceImages : [],
+        partsOfCostume : [
+          { part: 'jacket',
+            notes: 'needs to be cool',
+            materialOptions : [
+              { image : 'http://google.com',
+                cost : 34,
+                location : 'Britex',
+                link : undefined,
+                weight : 'medium'
+              }
+            ],
+            partsImageRefrence : []
+          }
+        ],
+      },
+      { character : 'Ferid',
+        refrenceImages : [],
+        partsOfCostume : [
+          { part: 'cloak',
+            notes: 'needs to be fab',
+            materialOptions : [
+              { image : 'http://google.com',
+                cost : 20,
+                location : 'Britex',
+                link : undefined,
+                weight : 'light'
+              }
+            ],
+            partsImageRefrence : []
+          }
+        ],
+      }
+    ],
+    lastLoggedIn : undefined
+  }
 // ---------------------------------------------------
 
 
@@ -25,5 +68,52 @@ return {
 
 //----------- PROJECT SCHEMA -----------
 
+// { projectName = 'Owari no Sereph' ,
+//   projectOwner = 'Chris',
+//   projectMembers = {
+//     owner : 'Chris',
+//     as7g6d78g9 : 'John'
+//   }
+//   eventDeadline = "AnimeExpo",
+//   eventDeadlineDate = "7/1/16",
+//   projectCreated = Date(),
+//   costumes = [
+//     { character : 'Guren',
+//       refrenceImages : [],
+//       partsOfCostume : [
+//         { part: 'jacket',
+//           notes: 'needs to be cool',
+//           materialOptions : [
+//             { image : 'http://google.com',
+//               cost : 34,
+//               location : 'Britex',
+//               link : undefined,
+//               weight : 'medium'
+//             }
+//           ],
+//           partsImageRefrence : []
+//         }
+//       ],
+//     },
+//     { character : 'Ferid',
+//       refrenceImages : [],
+//       partsOfCostume : [
+//         { part: 'cloak',
+//           notes: 'needs to be fab',
+//           materialOptions : [
+//             { image : 'http://google.com',
+//               cost : 20,
+//               location : 'Britex',
+//               link : undefined,
+//               weight : 'light'
+//             }
+//           ],
+//           partsImageRefrence : []
+//         }
+//       ],
+//     }
+//   ];
+//   lastLoggedIn = undefined;
+// }
 
 
