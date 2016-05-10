8e6127061d810c7267899ba777a0f72f674c4e2b
Christopher Decker <christopher.w.decker@gmail.com>
2016-05-10 14:15:40 -0700

    [feat] attempt to start adding params to the ULR for refrence

--- client/project/projectCtrl.js
+++ client/project/projectCtrl.js
@@ -1,11 +1,14 @@
 angular
   .module('meetup.project', [])
-  .controller('Project', ['$scope', 'ProjectFactory', function ($scope, ProjectFactory) {
+  .controller('Project', ['$scope', '$state', '$stateParams', 'ProjectFactory', function ($scope, $state, $stateParams, ProjectFactory) {
     $scope.owner = ProjectFactory.project.owner;
     $scope.project = ProjectFactory.project.projectName;
     $scope.deadline = ProjectFactory.project.eventDeadline;
     $scope.deadlineDate = ProjectFactory.project.eventDeadlineDate;
     $scope.costumes = ProjectFactory.project.costumes;
 
+    $stateParams.project = ProjectFactory.project.projectName
+    // $scope.project = "Owari No Serfeph";
+    console.log('----- $stateParams ----- ', $stateParams);
 
   }]);
