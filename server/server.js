c59bd36b39f28f8a8e6cf068631e50f6c05107b6
christopher decker <christopher.w.decker@gmail.com>
2016-05-17 09:24:37 -0700

    [notes] add notes to server in midleware after watching frontendmaster videos

--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
+
+// mongo db is currently setupf or local, change in future use
+// for local dev, use 'mongod://localhost/' + __current or new db__
+// a new db will be created if it does not exist
+mongoose.connect('mongodb://localhost/TeamUpCosplay');
+
+var TestSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean
+});
+// we are now accessing the 'TeamUpCosplay' db, and now we refrence the collection want as the first arg
+// if there is a second param, its assumed that it is a schema
+// without the second arg, it will assume its a refrence to the db collection
+var Testing = mongoose.model('localTest', TestSchema);
+Testing.create({
+    name:'JOHN',
+    completed: true
+}).then(function(err, test){
+    console.log('IT WORKED', err, todo);
+})
+
 
 require('./middleware.js')(app, express);
 // app.use(function(req, res) {
