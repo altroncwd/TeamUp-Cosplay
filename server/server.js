6c9caf2de2b036fe7886b5c4b183ceaac30774a0
Christopher Decker <christopher.w.decker@gmail.com>
2016-05-24 14:41:36 -0700

    [refactor] update and clear up middlwear server, and move notes to an md file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,10 @@
-var express = require('express');
 var app = require('./serverConfig.js');
 var server = require('http').createServer(app);
+
 console.log("Starting up the server");
 var PORT = process.env.PORT || 3000;
 
-    // if you want specific fields to be required or uniquie, you put them in an obj
-    // and then key the feilds
-    // name : {
-    //     type: string,
-    //     require: true
-    // }
 
-// require('./middleware.js')(app, express);
 
 server.listen(PORT);
-
 console.log("Server is now running at PORT:", PORT );
