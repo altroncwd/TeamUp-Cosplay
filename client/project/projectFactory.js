e6b7da9672498850390f37df9d12aeb0066a65dc
Christopher Decker <christopher.w.decker@gmail.com>
2016-05-24 20:14:30 -0700

    [refactor] adds minor changes and includes a error handler on the GET request

--- client/project/projectFactory.js
+++ client/project/projectFactory.js
@@ -1,5 +1,5 @@
 angular.module('meetup.project.factory', [])
-  .factory('ProjectFactory', ['$state', '$stateParams', function ($state, $stateParams) {
+  .factory('ProjectFactory', ['$state', '$stateParams', '$http', function ($state, $stateParams, $http) {
 // the factory should store data
 
   var project = {};
@@ -8,17 +8,25 @@ angular.module('meetup.project.factory', [])
   // Have a check on the stateParam, if the project is diffrent then whats loaded, run the function again to get the appropriate project, otherwise dont do the get request.
   console.log("I'm in the factory : ", $stateParams.project)
   var getProject = function () {
-      console.log("I'm in the factory");
+      console.log("about to look for our project : ", $stateParams.project );
       projectGet = '/project/' + this.project;
       $http({
         method: 'GET',
         url: projectGet
-      }). then(function(res) {
-        // remember res is the whole response, including the 400, the data property is what we actualy want/get back
-        project = res.data;
-      })
+      }). then(function(response) {
+        // remember response is the whole thing, including the 400, the data property is what we actualy want/get back
+
+        console.log("RESPONSE : ", response);
+        project = response.data;
+
+      }, function (response) {
+        // error response
+      }  )
     }
 
+  if( project == undefined || project.projectName !== $stateParams.project){
+    getProject();
+  }
 
 
 // TEMP TEST DATA: -----------------------------------
@@ -109,55 +117,3 @@ return {
 }
 
   }]);
-
-//----------- PROJECT SCHEMA -----------
-
-// { projectName = 'Owari no Sereph' ,
-//   projectOwner = 'Chris',
-//   projectMembers = {
-//     owner : 'Chris',
-//     as7g6d78g9 : 'John'
-//   }
-//   eventDeadline = "AnimeExpo",
-//   eventDeadlineDate = "7/1/16",
-//   projectCreated = Date(),
-//   costumes = [
-//     { character : 'Guren',
-//       refrenceImages : [],
-//       partsOfCostume : [
-//         { part: 'jacket',
-//           notes: 'needs to be cool',
-//           materialOptions : [
-//             { image : 'http://google.com',
-//               cost : 34,
-//               location : 'Britex',
-//               link : undefined,
-//               weight : 'medium'
-//             }
-//           ],
-//           partsImageRefrence : []
-//         }
-//       ],
-//     },
-//     { character : 'Ferid',
-//       refrenceImages : [],
-//       partsOfCostume : [
-//         { part: 'cloak',
-//           notes: 'needs to be fab',
-//           materialOptions : [
-//             { image : 'http://google.com',
-//               cost : 20,
-//               location : 'Britex',
-//               link : undefined,
-//               weight : 'light'
-//             }
-//           ],
-//           partsImageRefrence : []
-//         }
-//       ],
-//     }
-//   ];
-//   lastLoggedIn = undefined;
-// }
-
-
