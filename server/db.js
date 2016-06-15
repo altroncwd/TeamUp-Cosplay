633381904e1466b6461a52723c4b97b1ada558fc
Christopher Decker <christopher.w.decker@gmail.com>
2016-06-14 21:00:11 -0700

    [fix] clear up notes in the main db file

--- server/db.js
+++ server/db.js
@@ -6,19 +6,21 @@ console.log("Connecting to the Mongo DB");
 // for local dev, use 'mongod://localhost/' + __current or new db__
 // a new db will be created if it does not exist
 
-// var TestSchema = new mongoose.Schema({
-//     name: String,
-//     completed: Boolean
-// });
-// // we are now accessing the 'TeamUpCosplay' db, and now we refrence the collection want as the first arg
-// // if there is a second param, its assumed that it is a schema
-// // without the second arg, it will assume its a refrence to the db collection
-// var Testing = mongoose.model('localTest', TestSchema);
-// Testing.create({
-//     name:'JOHN',
-//     completed: true
-// }).then(function(err, test){
-//     console.log('IT WORKED', err, todo);
-// })
+    // var TestSchema = new mongoose.Schema({
+    //     name: String,
+    //     completed: Boolean
+    // });
+
+// we are now accessing the 'TeamUpCosplay' db, and now we refrence the collection want as the first arg
+// if there is a second param, its assumed that it is a schema
+// without the second arg, it will assume its a refrence to the db collection
+
+    // var Testing = mongoose.model('localTest', TestSchema);
+    // Testing.create({
+    //     name:'JOHN',
+    //     completed: true
+    // }).then(function(err, test){
+    //     console.log('IT WORKED', err, todo);
+    // })
 
 module.exports = db;
